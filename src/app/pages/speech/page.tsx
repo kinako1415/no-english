36b2aa67871a65143a82e7958de226d4355610ab
kinako1415 <Owata09@outlook.jp>
2024-10-06 22:11:58 +0900

    スタートボタンとストップボタンの切り替え

--- src/app/pages/speech/page.tsx
+++ src/app/pages/speech/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import "regenerator-runtime";
-import { useSpeechRecognition } from "react-speech-recognition";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import StartButton from "@/app/components/ui/startBotton";
 import { zenMaruGothic } from "@/app/fonts/zenFont";
 import StopButton from "@/app/components/ui/stopButton";
+import Image from "next/image";
 
 const Speech = () => {
   const [
@@ -12,8 +15,12 @@ const Speech = () => {
     setBrowserSupportsSpeechRecognition,
   ] = useState(false);
 
-  const { transcript, resetTranscript, browserSupportsSpeechRecognition } =
-    useSpeechRecognition();
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
 
   useEffect(() => {
     setBrowserSupportsSpeechRecognition(browserSupportsSpeechRecognition);
@@ -25,8 +32,13 @@ const Speech = () => {
 
   return (
     <div id="react-speech-recognition" className={zenMaruGothic.className}>
-      <StartButton>開始</StartButton>
-      <StopButton>停止</StopButton>
+      {listening ? (
+        <StopButton>停止</StopButton>
+      ) : (
+        <StartButton>開始</StartButton>
+      )}
+      <Image src="/man1.svg" alt="start" width={750} height={750} />
+      <Image src="/woman1.svg" alt="start" width={750} height={750} />
       <button type="button" onClick={() => resetTranscript()}>
         リセット
       </button>
