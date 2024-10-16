340bd8a4da579ea77ec20ffc820aa0123f5d52aa
kinako1415 <Owata09@outlook.jp>
2024-10-16 15:19:22 +0900

    login.tsxの作成

---
+++ src/app/pages/auth/login.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { supabase } from "@/app/lib/supabase";
+import { FormEvent, useState } from "react";
+
+export default function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSabmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+    if (error) {
+      console.error(error.message);
+    } else {
+      console.log("user signed up:", data);
+    }
+  };
+
+  return (
+    <form onSubmit={onSabmit}>
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button type="submit">login</button>
+    </form>
+  );
+}
