3cf3b692e5bad8fe702d4760127d96bc329b2aad
kinako1415 <Owata09@outlook.jp>
2024-10-17 12:58:33 +0900

    headerの固定

--- src/app/components/ui/header.tsx
+++ src/app/components/ui/header.tsx
@@ -4,18 +4,25 @@ import styled from "styled-components";
 const HeaderStyle = styled.header`
   position: fixed;
   display: flex;
-  justify-content: space-between;
+  align-items: center;
   width: 96%;
+  top: 30px;
   left: 0;
   right: 0;
   margin: 0 auto;
 `;
 
 const MenuStyle = styled.div`
-	width: 60px;
-	height: 60px;
-	background-color: aqua;
-`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 40px;
+  border-radius: 50px;
+  background-color: aqua;
+  right: 0;
+`;
 
 export default function Header() {
   return (
